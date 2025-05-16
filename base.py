6501ce07a7f2bb6cc9d388da2df7fe5d4ba9dac4
hizFin <cdwcdw34@gmail.com>
2025-05-16 11:58:58 +0900

    init

--- base.py
+++ base.py
@@ -1,6 +1,7 @@
 import threading
 import time
 
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
